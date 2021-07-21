151f22cf56e93c6fff418c676f49d0ebf622d7a3
Waterwheel <sonic870316@gmail.com>
2021-07-21 13:29:34 +0800

    fix: Fix warnings

--- src/components/BlockRender.tsx
+++ src/components/BlockRender.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../rootReducer';
 import keyDownHandler from '../keyDownHandler';
